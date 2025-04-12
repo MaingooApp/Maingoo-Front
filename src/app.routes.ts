320129916bd372fbaeeaf91234705ba8cff08684
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-04-12 18:29:48 +0200

    feat: add login component with routing and template integration

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,6 +3,7 @@ import { AppLayout } from './app/layout/component/app.layout';
 import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
+import { Login } from './app/pages/auth/login/login.component';
 
 export const appRoutes: Routes = [
     {
@@ -14,6 +15,7 @@ export const appRoutes: Routes = [
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
     },
+    {path: 'login', component:  Login},
     { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
     { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
