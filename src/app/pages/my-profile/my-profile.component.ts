70e34066b8170eaf3d4c59b9271087bae869e21f
Alex Guerrero <alexitowy@gmail.com>
2025-04-16 18:19:41 +0200

    feat(my-profile): implement profile form with validation and initial data loading

--- src/app/pages/my-profile/my-profile.component.ts
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -1,11 +1,66 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputMaskModule } from 'primeng/inputmask';
+import { FluidModule } from 'primeng/fluid';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputMaskModule,
+    ButtonModule,
+    CardModule,
+    FluidModule
+  ],
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.scss'
 })
 export class MyProfileComponent {
+  perfilForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.perfilForm = this.fb.group({
+      nombre: ['', Validators.required],
+      nif: ['', Validators.required],
+      direccion: ['', Validators.required],
+      telefono: ['', Validators.required],
+      telefono2: [''],
+      email: ['', [Validators.email]],
+      iban: [null]
+    });
+
+    this.cargarDatosIniciales();
+  }
+
+  cargarDatosIniciales() {
+    const data = {
+      nombre: 'Mi Negocio SL',
+      nif: 'B12345678',
+      direccion: 'Calle Falsa 123',
+      telefono: '600 123 456',
+      email: 'contacto@minegocio.com',
+    };
+
+    this.perfilForm.patchValue(data);
+  }
+
+  guardarPerfil() {
+    if (this.perfilForm.valid) {
+      const datos = this.perfilForm.value;
+      console.log('Guardando perfil:', datos);
+      // lógica de guardado (Firebase, etc.)
+    }
+  }
 
 }
