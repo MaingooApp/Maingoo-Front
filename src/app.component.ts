5ec72660a0144fb74074dbab6ec708296b96e118
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-11-10 13:57:27 +0100

    refactor: update application version in package.json and log version in AppComponent

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
+import packageJson from '../package.json';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule,
-        HttpClientModule,
-        ConfirmDialogModule,
-        ToastModule],
+    imports: [RouterModule, HttpClientModule, ConfirmDialogModule, ToastModule],
     templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor() {
+        console.warn('Version:', packageJson.version);
+    }
+}
