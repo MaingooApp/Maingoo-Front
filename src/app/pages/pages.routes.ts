b20b9f8792f581264ee8e7edf9715bf8f2c9c88f
Jariskan <sergio.marin.alvarez@gmail.com>
2025-11-13 13:08:39 +0100

    feat: implementar carga de documentos para análisis y eliminar componente de carga obsoleto

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
-import { UploadComponent } from './upload/upload.component';
 
 export default [
     { path: 'crud', component: Crud },
     { path: 'empty', component: Empty },
-    {path: 'upload', component: UploadComponent},
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
