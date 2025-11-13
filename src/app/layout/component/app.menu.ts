cac6b7aad3be248a589c331c3a4899f23e1a32f3
Jariskan <sergio.marin.alvarez@gmail.com>
2025-11-13 15:19:41 +0100

    feat: actualizar ícono del menú de facturas en el componente de menú

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -34,7 +34,7 @@ export class AppMenu {
                 label: this.translate.instant('menu.home'),
                 items: [
                     { label: this.translate.instant('menu.dashboard'), icon: 'pi pi-fw pi-home', routerLink: ['/'] },
-                    { label: this.translate.instant('menu.invoices'), icon: 'pi pi-fw pi-list', routerLink: ['/facturas'] },
+                    { label: this.translate.instant('menu.invoices'), icon: 'pi pi-fw pi-receipt', routerLink: ['/facturas'] },
                     { label: this.translate.instant('menu.suppliers'), icon: 'pi pi-fw pi-box', routerLink: ['/proveedores'] },
                     { label: this.translate.instant('menu.products'), icon: 'pi pi-fw pi-tags', routerLink: ['/productos'] },
                     { label: this.translate.instant('menu.documentation'), icon: 'pi pi-fw pi-file', routerLink: ['/docgenerator'] },
