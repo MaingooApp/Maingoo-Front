2e7d0fb185f7bfdb78d8c33251c3e89ee425c4d8
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-11-18 15:53:09 +0100

    feat: add product detail view and routing; implement product service methods for fetching products

--- src/app/pages/productos/productos.component.spec.ts
+++ src/app/pages/productos/productos.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProductosComponent } from './productos.component';
 
@@ -8,7 +9,7 @@ describe('ProductosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductosComponent]
+      imports: [ProductosComponent, RouterTestingModule]
     })
     .compileComponents();
 
