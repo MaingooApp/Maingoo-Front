16c64aaaf2ccd14f16630ef37fc7fbbb8ea401b5
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-11-26 09:51:08 +0100

    refactor(3): change name folder pages for features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { roleGuard } from './core/guard/role-guard.guard';
 import { AppLayout } from './layout/component/app.layout';
-import { Dashboard } from './pages/dashboard/dashboard';
-import { Landing } from './pages/landing/landing';
-import { Notfound } from './pages/notfound/notfound';
-import { SupplierComponent } from './pages/supplier/supplier.component';
-import { DocGeneratorComponent } from './pages/doc-generator/doc-generator.component';
-import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Landing } from './features/landing/landing';
+import { Notfound } from './features/notfound/notfound';
+import { SupplierComponent } from './features/supplier/supplier.component';
+import { DocGeneratorComponent } from './features/doc-generator/doc-generator.component';
+import { MyProfileComponent } from './features/my-profile/my-profile.component';
 
 export const appRoutes: Routes = [
     {
@@ -14,11 +14,11 @@ export const appRoutes: Routes = [
         component: AppLayout,
         children: [
             { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./pages/uikit/uikit.routes') },
-            { path: 'pages', loadChildren: () => import('./pages/pages.routes') },
-            { path: 'facturas', loadChildren: () => import('./pages/facturas/invoice.routes') },
+            { path: 'uikit', loadChildren: () => import('./features/uikit/uikit.routes') },
+            { path: 'pages', loadChildren: () => import('./features/pages.routes') },
+            { path: 'facturas', loadChildren: () => import('./features/facturas/invoice.routes') },
             { path: 'proveedores', component: SupplierComponent },
-            { path: 'productos', loadChildren: () => import('./pages/productos/product.routes') },
+            { path: 'productos', loadChildren: () => import('./features/productos/product.routes') },
             { path: 'docgenerator', component: DocGeneratorComponent },
             { path: 'miperfil', component: MyProfileComponent }
         ],
@@ -26,6 +26,6 @@ export const appRoutes: Routes = [
     },
     { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./pages/auth/auth.routes') },
+    { path: 'auth', loadChildren: () => import('./features/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
