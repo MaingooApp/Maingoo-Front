b848a563499a42bd2b654428df1fddd921ac988d
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-11-26 21:37:20 +0100

    refactor: move Supplier interfaces to a separate file for better organization

--- src/app/features/supplier/services/supplier.service.ts
+++ src/app/features/supplier/services/supplier.service.ts
@@ -2,45 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BaseHttpService } from '../../../core/services/base-http.service';
 import { environment } from '../../../../environments/environment';
-
-export interface Supplier {
-    id?: string;
-    name: string;
-    cifNif: string;
-    address?: string | null;
-    phoneNumber?: string | null;
-    commercialName?: string | null;
-    commercialPhoneNumber?: string | null;
-    deliveryDays?: string | null;
-    minPriceDelivery?: number | null;
-    sanitaryRegistrationNumber?: string | null;
-    createdAt?: string;
-    updatedAt?: string;
-}
-
-export interface CreateSupplierDto {
-    name: string;
-    cifNif: string;
-    address?: string;
-    phoneNumber?: string;
-    commercialName?: string;
-    commercialPhoneNumber?: string;
-    deliveryDays?: string;
-    minPriceDelivery?: number;
-    sanitaryRegistrationNumber?: string;
-}
-
-export interface UpdateSupplierDto {
-    name?: string;
-    cifNif?: string;
-    address?: string;
-    phoneNumber?: string;
-    commercialName?: string;
-    commercialPhoneNumber?: string;
-    deliveryDays?: string;
-    minPriceDelivery?: number;
-    sanitaryRegistrationNumber?: string;
-}
+import { CreateSupplierDto, Supplier, UpdateSupplierDto } from '../interfaces/supplier.interface';
 
 @Injectable({
     providedIn: 'root'
