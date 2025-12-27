4b307f1bd4438a45ea99e0f8df01a3bd7d89959b
Jariskan <sergio.marin.alvarez@gmail.com>
2025-12-27 18:33:55 +0100

    Remove dashboard placeholder content and article component styleUrls.

--- src/app/features/articles/pages/list/articles.component.ts
+++ src/app/features/articles/pages/list/articles.component.ts
@@ -6,6 +6,5 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent {}
