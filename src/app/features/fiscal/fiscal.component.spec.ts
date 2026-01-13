8d70f8464f035ef958b0c50ee3a138c42638e361
Jariskan <sergio.marin.alvarez@gmail.com>
2026-01-13 09:34:43 +0100

    refactor: rename doc-generator directory to fiscal

--- src/app/features/fiscal/fiscal.component.spec.ts
+++ src/app/features/fiscal/fiscal.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DocGeneratorComponent } from './doc-generator.component';
+import { DocGeneratorComponent } from './fiscal.component';
 
 describe('DocGeneratorComponent', () => {
   let component: DocGeneratorComponent;
