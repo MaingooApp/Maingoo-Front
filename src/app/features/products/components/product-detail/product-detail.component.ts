e1e6c70cbdab02eb33ca6ea26749b9ef1de4cf5b
Jariskan <sergio.marin.alvarez@gmail.com>
2026-01-16 23:27:03 +0100

    refactor: Remove optional chaining from product and invoice templates and unused `IconComponent` import.

--- src/app/features/products/components/product-detail/product-detail.component.ts
+++ src/app/features/products/components/product-detail/product-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common'; // For ngIf, ngFor, date, currency
 import { ButtonModule } from 'primeng/button';
 import { Product, Invoice } from '@app/core/interfaces/Invoice.interfaces';
-import { IconComponent } from '../../../../shared/components/icon/icon.component';
+
 import { ProductAttributesComponent } from './components/product-attributes/product-attributes.component';
 import { ProductInfoComponent } from './components/product-info/product-info.component';
 import { ProductPriceChartComponent } from './components/product-price-chart/product-price-chart.component';
@@ -14,7 +14,6 @@ import { ProductInvoicesComponent } from './components/product-invoices/product-
   imports: [
     CommonModule,
     ButtonModule,
-    IconComponent,
     ProductAttributesComponent,
     ProductInfoComponent,
     ProductPriceChartComponent,
