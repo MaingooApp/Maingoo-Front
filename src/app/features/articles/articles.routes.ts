717be0882a0c6b6d470a3738c8ac165494e80090
Jariskan <sergio.marin.alvarez@gmail.com>
2026-01-19 09:47:29 +0100

    feat: Introduce new articles and articles-card components, restructuring the articles feature and replacing the previous list page.

--- src/app/features/articles/articles.routes.ts
+++ src/app/features/articles/articles.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ArticlesComponent } from './pages/list/articles.component';
+import { ArticlesComponent } from './articles.component';
 
 const articlesRoutes: Routes = [
   { path: '', component: ArticlesComponent }
