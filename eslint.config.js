a4a8c715a37278995097d796b09361c9f568d6a8
Jariskan <sergio.marin.alvarez@gmail.com>
2026-05-18 14:50:59 +0200

    chore: configure linting and style foundations

--- eslint.config.js
+++ eslint.config.js
@@ -1,101 +1,82 @@
-export default {
-  root: true,
-  ignorePatterns: ['**/dist/**'],
-  plugins: ['prettier'],
-  extends: ['prettier'],
-  rules: {
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-      { blankLine: 'any', prev: ['case', 'default'], next: 'break' },
-      { blankLine: 'any', prev: 'case', next: 'case' },
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: 'block', next: '*' },
-      { blankLine: 'always', prev: '*', next: 'block' },
-      { blankLine: 'always', prev: 'block-like', next: '*' },
-      { blankLine: 'always', prev: '*', next: 'block-like' },
-      { blankLine: 'always', prev: ['import'], next: ['const', 'let', 'var'] }
-    ]
+import eslint from '@eslint/js';
+import angular from '@angular-eslint/eslint-plugin';
+import angularTemplate from '@angular-eslint/eslint-plugin-template';
+import angularTemplateParser from '@angular-eslint/template-parser';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', '.angular/**', 'coverage/**', 'node_modules/**']
   },
-  overrides: [
-    {
-      files: ['*.ts'],
+  {
+    files: ['**/*.ts'],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      '@angular-eslint': angular
+    },
+    processor: angularTemplate.processors['extract-inline-html'],
+    languageOptions: {
       parserOptions: {
-        project: ['tsconfig.json', 'e2e/tsconfig.json'],
-        createDefaultProgram: true
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@angular-eslint/recommended',
-        'plugin:@angular-eslint/template/process-inline-templates',
-        'prettier'
-      ],
-      rules: {
-        '@angular-eslint/component-selector': [
-          'error',
-          {
-            type: 'element',
-            prefix: 'p',
-            style: 'kebab-case'
-          }
-        ],
-        '@angular-eslint/directive-selector': [
-          'error',
-          {
-            type: 'attribute',
-            prefix: 'p',
-            style: 'camelCase'
-          }
-        ],
-        '@angular-eslint/component-class-suffix': [
-          'error',
-          {
-            suffixes: ['']
-          }
-        ],
-        '@angular-eslint/template/eqeqeq': [
-          'error',
-          {
-            allowNullOrUndefined: true
-          }
-        ],
-        '@angular-eslint/no-host-metadata-property': 'off',
-        '@angular-eslint/no-output-on-prefix': 'off',
-        '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-inferrable-types': 'off',
-        'arrow-body-style': ['error', 'as-needed'],
-        curly: 0,
-        '@typescript-eslint/member-ordering': [
-          'error',
-          {
-            default: [
-              'public-static-field',
-              'static-field',
-              'instance-field',
-              'public-instance-method',
-              'public-static-field'
-            ]
-          }
-        ],
-        'no-console': 0,
-        'prefer-const': 0
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
       }
     },
-    {
-      files: ['*.html'],
-      extends: ['plugin:@angular-eslint/template/recommended', 'prettier'],
-      rules: {}
-    },
-    {
-      files: ['*.js'],
-      rules: {
-        parserOptions: {
-          allowImportExportEverywhere: true
+    rules: {
+      ...angular.configs.recommended.rules,
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case'
         }
-      }
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase'
+        }
+      ],
+      '@angular-eslint/component-class-suffix': 'off',
+      '@angular-eslint/no-empty-lifecycle-method': 'off',
+      '@angular-eslint/no-output-on-prefix': 'off',
+      '@angular-eslint/no-output-native': 'off',
+      '@angular-eslint/use-lifecycle-interface': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-case-declarations': 'off',
+      'no-console': 'off',
+      'no-control-regex': 'off',
+      'prefer-const': 'off'
     }
-  ]
-};
+  },
+  {
+    files: ['**/*.html'],
+    plugins: {
+      '@angular-eslint/template': angularTemplate
+    },
+    languageOptions: {
+      parser: angularTemplateParser
+    },
+    rules: {
+      ...angularTemplate.configs.recommended.rules,
+      ...angularTemplate.configs.accessibility.rules,
+      '@angular-eslint/template/eqeqeq': [
+        'error',
+        {
+          allowNullOrUndefined: true
+        }
+      ],
+      '@angular-eslint/template/click-events-have-key-events': 'off',
+      '@angular-eslint/template/elements-content': 'off',
+      '@angular-eslint/template/interactive-supports-focus': 'off',
+      '@angular-eslint/template/label-has-associated-control': 'off',
+      '@angular-eslint/template/no-autofocus': 'off'
+    }
+  },
+  prettier
+);
