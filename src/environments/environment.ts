df5c73dc652e2198193150de9c0656ffa7afbdf6
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2026-06-08 22:22:05 +0200

    chore: point environment backend url to production gateway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // urlBackend: 'https://ms-gateway.salmonriver-6a199de3.westeurope.azurecontainerapps.io/'
-  urlBackend: 'http://localhost:3000/'
+  urlBackend: 'https://ms-gateway.salmonriver-6a199de3.westeurope.azurecontainerapps.io/'
 };
